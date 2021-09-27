ee1123612f72f5fca622f8a388daadab852204b4
Jesus <jsalas@nettrim.es>
2021-09-27 10:27:23 +0200

    FilmController

---
+++ FilmRest/Controllers/FilmController.cs
@@ -0,0 +1,20 @@
+﻿using FilmRest.Services;
+using Microsoft.AspNetCore.Mvc;
+using VideoClubCore.Domain;
+using VideoClubCore.Services;
+
+namespace FilmRest.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+        private static IFilmService _filmService = new FilmServices();
+        [HttpGet("{id}")]
+        public Film GetMovie(string id)
+        {
+            var film = _filmService.GetById(id);
+            return film;
+        }
+    }
+}
---
+++ VideoClubInfra/HTTP/RestFullFilmService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using VideoClubCore.Domain;
+using VideoClubCore.Services;
+
+namespace VideoClubInfra.HTTP
+{
+    class RestFullFilmService : IFilmService
+    {
+        public List<Film> GetByCategory(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Film GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
