249b235c0b19f025050ddb1e6c9963c192f61e43
Jesus <jsalas@nettrim.es>
2021-09-27 11:26:19 +0200

    infra and console

---
+++ VideoClubConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using FilmRest.Services;
+using System;
+using VideoClubCore.Services;
+
+namespace VideoClubConsole
+{
+    class Program
+    {
+        private static IFilmService _filmService = new FilmServices();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seleccione la pelicula que desee ver: AA, AB, AC");
+            var film = _filmService.GetById(Console.ReadLine());
+            Console.WriteLine($"Nombre: {film.Name}");
+            Console.WriteLine($"Category: {film.Category}");
+        }
+    }
+}
--- VideoClubInfra/HTTP/RestFullFilmService.cs
+++ VideoClubInfra/HTTP/RestFullFilmService.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Text;
 using VideoClubCore.Domain;
 using VideoClubCore.Services;
 
@@ -7,6 +10,7 @@ namespace VideoClubInfra.HTTP
 {
     class RestFullFilmService : IFilmService
     {
+        private Film _film;
         public List<Film> GetByCategory(string category)
         {
             throw new NotImplementedException();
@@ -14,7 +18,15 @@ namespace VideoClubInfra.HTTP
 
         public Film GetById(string id)
         {
-            throw new NotImplementedException();
+            string apiUrl = "https://localhost:44306/film";
+            using (WebClient client = new WebClient())
+            {
+                client.Headers["Content-type"] = "application/json";
+                client.Encoding = Encoding.UTF8;
+                string json = client.DownloadString($"{apiUrl}/{_film.Id}");
+                var film = JsonConvert.DeserializeObject<Film>(json);
+                return film;
+            }
         }
     }
 }
